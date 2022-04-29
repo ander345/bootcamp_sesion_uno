a23fc0f602e90eae4fb987f13a18fe374ce4b1d2
Anderson <jcasas@bancodebogota.com.co>
2022-04-28 22:51:44 -0500

    Agregando paquete con sesion 2 de funcion: Para este ejercicio tendréis que crear una función que reciba un precio y devuelva el precio con el IVA incluido.

--- src/com/openbootcamp/sesion/dos/mainfuncion.java
+++ src/com/openbootcamp/sesion/dos/mainfuncion.java
@@ -1,9 +1,13 @@
 package com.openbootcamp.sesion.dos;
 
+import java.util.Scanner;
+
 public class mainfuncion {
 
     public static void main(String[] args) {
-        double precio = 1152564;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce valor: ");
+        double precio = scanner.nextInt();
         double result = iva(precio);
         System.out.println("precio sin iva: " + precio);
         System.out.println("precio con iva " + result);
